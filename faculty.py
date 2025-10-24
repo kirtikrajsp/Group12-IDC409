f40cedc54120338254c50706a4e52bd2de9be06c
kirtikrajsp <ms22254@iisermohali.ac.in>
2025-10-24 13:03:14 +0000

    .

--- faculty.py
+++ faculty.py
@@ -7,3 +7,13 @@ import numpy as np
 import tabulate
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+print("Fetching main faculty page...")
+data = requests.get('https://www.iiserkol.ac.in/web/en/people/faculty/dbs/')
+soup = BeautifulSoup(data.content, "html.parser")
+
+# Collecting faculty profile URLs
+urls = [link['href'] for link in soup.find_all('a')]
+urls = [url for url in urls if '/web/en/people/faculty/dbs/' in url]
+link = 'https://www.iiserkol.ac.in'
+faculty_websites = list(set([link + url for url in urls]))
+print(f"Found {len(faculty_websites)} faculty profile URLs.")
