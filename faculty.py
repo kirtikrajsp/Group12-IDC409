503d56309d7dc2d8e55339d1a26429a49f9d132f
kirtikrajsp <ms22254@iisermohali.ac.in>
2025-10-24 17:16:40 +0530

    import tools

--- faculty.py
+++ faculty.py
@@ -1,3 +1,9 @@
-x=10
-y=1
-print (x-y)
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3
+import re
+import numpy as np
+import tabulate
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
